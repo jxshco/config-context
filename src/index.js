4d5249ae03a429d90455cad982d8d3e44ce646fa
Josh Sturrock <jxshco@gmail.com>
2018-10-15 21:04:27 +0100

    Added Config

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+
+const ConfigContext = React.createContext();
+
+export const ConfigProvider = ({ children, ...rest }) => (
+  <ConfigContext.Provider value={{ ...rest }}>{children}</ConfigContext.Provider>
+);
+
+ConfigProvider.propTypes = {
+  children: React.Node.isRequired,
+};
+
+export function withConfig(Component) {
+  return function ConnectedComponent(props) {
+    return (
+      <ConfigContext.Consumer>
+        {config => <Component {...props} config={config} />}
+      </ConfigContext.Consumer>
+    );
+  };
+}
+
+export const ConfigConsumer = ConfigContext.Consumer;
